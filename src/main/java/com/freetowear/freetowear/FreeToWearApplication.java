b2eec08527e40de4a70c85347516ae6679c372a7
almeidafm <132709094+almeidafm@users.noreply.github.com>
2026-04-27 17:01:05 -0300

    SpringApplication

---
+++ src/main/java/com/freetowear/freetowear/FreeToWearApplication.java
@@ -0,0 +1,13 @@
+package com.freetowear.freetowear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeToWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeToWearApplication.class, args);
+	}
+
+}
