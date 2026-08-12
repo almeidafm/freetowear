f25f5accd51c3d083dcd837d587046e30479b90a
almeidafm <132709094+almeidafm@users.noreply.github.com>
2026-08-12 18:18:05 -0300

    feat: script for listing all categories on product page creation

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,6 +2,7 @@ fetch('/account/address')
     .then(res => res.json())
     .then(addresses => {
         const select = document.querySelector('select[name="idAddress"]');
+
         addresses.forEach(address => {
             const option = document.createElement('option');
             option.value = address.id;
@@ -10,6 +11,20 @@ fetch('/account/address')
             select.appendChild(option);
         });
     });
+
+fetch('/category')
+    .then(res => res.json())
+    .then(categories => {
+        const select = document.querySelector('select[name="categoryId"]');
+
+        categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category.id;
+            option.text = category.name;
+            select.appendChild(option);
+        });
+    });
+
 fetch('/order/cart')
     .then(res => res.json())
     .then(order => {
