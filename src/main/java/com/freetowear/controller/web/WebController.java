48780c6f8e0b307b274c637769c77df24b135b10
almeidafm <132709094+almeidafm@users.noreply.github.com>
2026-09-16 17:37:42 -0300

    feat: add missing html files

--- src/main/java/com/freetowear/controller/web/WebController.java
+++ src/main/java/com/freetowear/controller/web/WebController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class WebController {
